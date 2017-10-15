0e7f51ec17a7f56ec0d1c74211ca50ee33541969
Scott Meade <scott@synapsoftware.com>
2017-10-15 14:59:59 -0600

    Views: “templates” are function definitions
    Temporarily delete lib/hello_web/templates/page/index.html.eex file and
    add render("index.html", assigns) function to HelloWeb.PageView.

--- lib/hello_web/views/page_view.ex
+++ lib/hello_web/views/page_view.ex
@@ -3,6 +3,10 @@ defmodule HelloWeb.PageView do
 
   # “templates” in Phoenix are really just function definitions on their view module
   # https://hexdocs.pm/phoenix/views.html#rendering-templates
+  # 
+  # At compile-time, Phoenix precompiles all *.html.eex templates and turns them into 
+  # render/2 function clauses on their respective view modules. 
+  # At runtime, all templates are already loaded in memory. 
   def render("index.html", assigns) do
     "rendering from HelloWeb.PageView render function with assigns #{inspect Map.keys(assigns)}"
   end
